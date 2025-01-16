808a6793fc54aeb2f0e9222dce5beaa1ba8fc26c
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2025-01-16 18:44:12 +0900

    display_one_contact追加

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -7,9 +7,17 @@ class Contact
 {
 private:
 	std::string first_name;
+	std::string last_name;
+	std::string nickname;
+	std::string phone_number;
+	std::string darkest_secret;
 public:
 	Contact();
 	std::string get_first_name() const;
+	std::string get_last_name() const;
+	std::string get_nickname() const;
+	std::string get_phone_number() const;
+	std::string get_darkest_secret() const;
 };
 
 #endif
