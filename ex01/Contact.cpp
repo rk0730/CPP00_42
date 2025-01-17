63a7b04dc51b348948d298c93528289a8597f986
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2025-01-17 16:53:50 +0900

    privateに_付与　並べ替え

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -2,59 +2,59 @@
 
 Contact::Contact()
 {
-	first_name = "";
-	last_name = "";
-	nickname = "";
-	phone_number = "";
-	darkest_secret = "";
+	_first_name = "";
+	_last_name = "";
+	_nickname = "";
+	_phone_number = "";
+	_darkest_secret = "";
 }
 
 std::string Contact::get_first_name() const
 {
-	return first_name;
+	return _first_name;
 }
 
 std::string Contact::get_last_name() const
 {
-	return last_name;
+	return _last_name;
 }
 
 std::string Contact::get_nickname() const
 {
-	return nickname;
+	return _nickname;
 }
 
 std::string Contact::get_phone_number() const
 {
-	return phone_number;
+	return _phone_number;
 }
 
 std::string Contact::get_darkest_secret() const
 {
-	return darkest_secret;
+	return _darkest_secret;
 }
 
 void Contact::set_first_name(std::string first_name)
 {
-	this->first_name = first_name;
+	_first_name = first_name;
 }
 
 void Contact::set_last_name(std::string last_name)
 {
-	this->last_name = last_name;
+	_last_name = last_name;
 }
 
 void Contact::set_nickname(std::string nickname)
 {
-	this->nickname = nickname;
+	_nickname = nickname;
 }
 
 void Contact::set_phone_number(std::string phone_number)
 {
-	this->phone_number = phone_number;
+	_phone_number = phone_number;
 }
 
 void Contact::set_darkest_secret(std::string darkest_secret)
 {
-	this->darkest_secret = darkest_secret;
+	_darkest_secret = darkest_secret;
 }
